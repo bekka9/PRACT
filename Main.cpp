560345879f78bad51d0c59391d238c4b5b449f12
bekka9 <57871070+bekka9@users.noreply.github.com>
2021-03-04 11:16:15 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -124,6 +124,384 @@ void RunTask1(int episodes)
 	std::cout << std::endl << "Arithmetic mean of 10 ep: " << s / 10 << std::endl;
 }
 
+void matchTemplate() {
+	IplImage* src = cvLoadImage(monstImage, 1),
+		* templ = cvLoadImage(finnImage, 1),
+		* ftmp[6];
+	assert(src != 0);
+	assert(templ != 0);
+
+	int patchx = templ->width;
+	int patchy = templ->height;
+	int iwidth = src->width - patchx + 1;
+	int iheight = src->height - patchy + 1;
+
+	for (int i = 0; i < 6; ++i) {
+		ftmp[i] = cvCreateImage(cvSize(iwidth, iheight), 32, 1);
+	}
+
+	for (int i = 0; i < 6; ++i) {
+		cvMatchTemplate(src, templ, ftmp[i], i);
+		double minval, maxval;
+		CvPoint minLoc, maxLoc;
+		cvMinMaxLoc(ftmp[i], &minval, &maxval, &minLoc, &maxLoc, 0);
+		cvNormalize(ftmp[i], ftmp[i], 1, 0, CV_MINMAX);
+		cvRectangle(src, maxLoc, cvPoint(maxLoc.x + templ->width - 1, maxLoc.y + templ->height - 1), CV_RGB(255, 0, 0), 1, 8);
+		cvRectangle(ftmp[i], maxLoc, cvPoint(maxLoc.x + templ->width - 1, maxLoc.y + templ->height - 1), CV_RGB(255, 0, 0), 1, 8);
+	}
+
+	cvShowImage("Template", templ);
+	cvShowImage("Image", src);
+	cvShowImage("0", ftmp[0]);
+	cvShowImage("1", ftmp[1]);
+	cvShowImage("2", ftmp[2]);
+	cvShowImage("3", ftmp[3]);
+	cvShowImage("4", ftmp[4]);
+	cvShowImage("5", ftmp[5]);
+
+	cvWaitKey(0);
+}
+
+void cont_det() {
+	// имя картинки задаётся первым параметром
+	const char* filename = "./assets/012.jpg";
+	// получаем картинку
+	IplImage* image = cvLoadImage(filename, 1);
+	assert(image != 0);
+
+	cvNamedWindow("origianl", CV_WINDOW_AUTOSIZE);
+
+	// для хранения изображения
+	IplImage* gray = 0, * bin = 0, * dst = 0;
+
+	// показываем картинку
+	cvShowImage("origianl", image);
+
+	// создание изображений
+	dst = cvCloneImage(image);
+	gray = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+	bin = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+
+	cvConvertImage(image, gray, CV_BGR2GRAY);
+	cvCanny(gray, bin, 50, 200, 3);
+
+	cvNamedWindow("cvCanny");
+	cvShowImage("cvCanny", bin);
+
+	// для хранения контуров
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	CvSeq* contours = 0, * seq = 0;
+
+	// находим контуры ..CV_CHAIN_CODE CV_CHAIN_APPROX_SIMPLE ---- CV_RETR_LIST
+	int contoursCont = cvFindContours(bin, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+	printf("[i] contours: %d \n", contoursCont);
+
+	// нарисуем контуры
+	for (seq = contours; seq != 0; seq = seq->h_next) {
+		cvDrawContours(image, seq, CV_RGB(255, 216, 0), CV_RGB(0, 0, 250), 0, 1, 4); // рисуем контур
+	}
+
+	cvNamedWindow("contours");
+	cvShowImage("contours", image);
+
+	// ждём нажатия клавиши
+	cvWaitKey(0);
+
+	// освобождаем ресурсы
+	//cvReleaseImage(&image);
+	//cvDestroyAllWindows();
+}
+void RunTask5_cd(int episodes)
+{
+
+	try
+	{
+		game->loadConfig(path + "\\scenarios\\task5.cfg");
+
+		game->init();
+
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
+	std::vector<double> action;
+	cv::Mat labels;
+	for (auto i = 0; i < episodes; i++)
+	{
+		game->newEpisode();
+		std::cout << "Episode #" << i + 1 << std::endl;
+
+		while (!game->isEpisodeFinished())
+		{
+			//версия с кластерами: 
+
+			const auto& gamestate = game->getState();
+
+			std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
+
+
+			cv::Mat img = screenBuff; //скрин экрана
+
+			cv::Mat image = screenBuff;
+			cv::Mat dst;
+			cv::Canny(img, dst, 10, 180, 3, false);
+
+			cv::imshow("cvCanny", dst);
+
+			IplImage * gray = 0, * bin = 0;
+			IplImage tmp = IplImage(img);//added code
+			CvArr* arr = (CvArr*)&tmp;
+
+			// создание изображени
+			//dst = cvCloneImage(arr);
+			gray = cvCreateImage(cvGetSize(arr), IPL_DEPTH_8U, 1);
+			bin = cvCreateImage(cvGetSize(arr), IPL_DEPTH_8U, 1);
+
+			cvConvertImage(arr, gray, CV_BGR2GRAY);
+			cvCanny(gray, bin, 50, 200, 3);
+			//cvShowImage("cvCanny", bin);
+
+			CvMemStorage* storage = cvCreateMemStorage(0);
+			CvSeq* contours = 0, * seq = 0;
+
+
+
+			cv::extractChannel(screenBuff, greyscale, 2);
+
+			cv::threshold(greyscale, greyscale, 230, 255, cv::THRESH_BINARY_INV);
+
+			std::vector<cv::Point2f> data(0);
+
+			for (int l = 0; l < 640; l++)
+			{
+				for (int j = 100; j < 410; j++)
+				{
+					if ((int)greyscale.at<unsigned char>(j, l) == 0) {
+						data.push_back(cv::Point2f(l, j));
+
+					}
+				}
+
+			}
+			bool stena_s_prava = false;
+			bool stena_s_leva = false;
+			for (int l = 340; l < 370; l++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					if ((int)dst.at<unsigned char>(j, l) == 255) {
+						stena_s_prava = true;
+						break;
+					}
+				}
+
+			}
+			for (int l = 270; l < 300; l++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					if ((int)dst.at<unsigned char>(j, l) == 255) {
+						stena_s_leva = true;
+						break;
+					}
+				}
+
+			}
+
+			
+
+			std::vector<cv::Point2f> centers;
+			
+			cv::imshow("img", img);
+			if (data.size() > 3) {
+				greyscale.convertTo(greyscale, CV_32F);
+
+				cv::kmeans(data, 4, labels, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0), 3, cv::KMEANS_RANDOM_CENTERS, centers);
+				greyscale.convertTo(greyscale, CV_8UC1);
+				for (int i = 0; i < 4; i++)
+				{
+					//cv::circle(greyscale, centers[i], 4, cv::Scalar(200, 0, 0), 5);
+
+
+					for (int i = 0; i < centers.size(); i++)
+					{
+						cv::Point c = centers[i];
+						//cv::putText(image, "AID", cv::Point(c.x - 20, c.y - 30), 1, 1, COLOR_GREEN);
+						cv::rectangle(image, cv::Point(c.x - 20, c.y - 30), cv::Point(c.x + 40, c.y + 30), COLOR_GREEN, 1);
+					}
+					cv::circle(greyscale, centers[0], 4, cv::Scalar(200, 0, 0), 5);
+					
+					//
+					if (centers[i].x < 320 - 10) { //
+						game->makeAction({ 0, 2 });
+					}
+					else if (centers[i].x > 320 + 33) { //
+						game->makeAction({ 2, 0 });
+
+					}
+					if (stena_s_prava == true) {
+						game->makeAction({ 11, 0 });
+					}
+					else if (stena_s_leva == true) {
+						game->makeAction({ 0, 12 });
+
+					}
+
+
+					else if ((centers[i].x > 310) && (centers[i].x < 330))
+					{
+						game->makeAction({ 1, 0 });
+
+					}
+
+					else if ((centers[i].x > 319) && (centers[i].x < 360)) {
+						game->makeAction({ 0, 1 });
+
+					}
+
+
+					// 1008.3
+					/*if (centers[i].x < 320 - 10) { //
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+					}
+					else if (centers[i].x > 320 + 33) { //
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+
+
+					}
+					if (stena_s_prava == true) {
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+
+
+						game->makeAction({ 1, 0 });
+						game->makeAction({ 1, 0 });
+
+						game->makeAction({ 1, 0 });
+						
+					}
+					else if (stena_s_leva == true) {
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+
+
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+
+
+						game->makeAction({ 0, 1 });
+						game->makeAction({ 0, 1 });
+
+					}
+					
+
+
+					else if ((centers[i].x > 310) && (centers[i].x < 330))
+					{
+						game->makeAction({ 1, 0 });
+
+					}
+
+					else if ((centers[i].x > 319) && (centers[i].x < 360)) {
+						game->makeAction({ 0, 1 });
+
+					}*/
+
+				
+										
+				}
+			}
+				
+			
+			
+			else {
+				//game->makeAction({ 1, 0 });
+				//game->makeAction({ 0, 1 });
+
+				bool stena_s_prava = false;
+				bool stena_s_leva = false;
+				for (int l = 330; l < 560; l++)
+				{
+					for (int j = 0; j < 10; j++)
+					{
+						if ((int)dst.at<unsigned char>(j, l) == 255) {
+							stena_s_prava = true;
+							break;
+						}
+					}
+
+				}
+				for (int l = 60; l < 310; l++)
+				{
+					for (int j = 0; j < 10; j++)
+					{
+						if ((int)dst.at<unsigned char>(j, l) == 255) {
+							stena_s_leva = true;
+							break;
+						}
+					}
+
+				}
+				if (stena_s_prava == true) {
+					for (int i = 0; i < 80; i++) {
+						game->makeAction({ 1, 0 });
+					}
+				}
+				else if (stena_s_leva == true) {
+					for (int i = 0; i < 80; i++) {
+						game->makeAction({ 0, 1 });
+					}
+
+				}
+				else {
+					game->makeAction({ 0, 0 });
+
+				}
+			}
+			
+			
+			
+			// находим контуры
+			int contoursCont = cvFindContours(bin, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+			printf("[i] contours: %d \n", contoursCont);
+
+			// нарисуем контуры
+			for (seq = contours; seq != 0; seq = seq->h_next) {
+				cvDrawContours(arr, seq, CV_RGB(255, 216, 0), CV_RGB(0, 0, 250), 0, 1, 4); // рисуем контур
+			}
+			cv::imshow("img", img);
+			//cvNamedWindow("contours");
+			cvShowImage("contours", arr);
+			cv::imshow("Output Window", greyscale);
+
+			
+
+			cv::waitKey(1);
+
+		}
+		s = s + game->getTotalReward();
+
+		std::cout << std::endl << game->getTotalReward() << std::endl;
+	}
+
+	std::cout << std::endl << "Arithmetic mean of 10 ep: " << s / 10 << std::endl;
+	
+}
+
 void RunTask2(int episodes)
 {
 	try
